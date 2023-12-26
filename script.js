e1318c2e55bd2c99b24d683bf51aed4900a929a0
Barry Hammer <barryhammer101@gmail.com>
2023-12-26 15:13:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"), {
+	strings: ["Senior QA Engineer", "Frontend Developer", "Technical Writer"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+}) 
